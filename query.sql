f9eee6e79fe5316fde97830af9ac99717cbbf7c1
askadiliya <askadiliyan@gmail.com>
2026-02-25 16:59:10 +0700

    feat: implement transaction order item preview query using product and variant data

--- query.sql
+++ query.sql
@@ -1,26 +1,70 @@
 SELECT 
-    p.id_product, 
-    p.product_name, 
-    p.base_price, 
-    c.category_name,
-    ARRAY_AGG(DISTINCT v.variant_name) FILTER (WHERE v.variant_name IS NOT NULL) AS "available_variants"
+    p.product_name,
+    pv.variant_name,
+    (p.base_price + pv.additional_price) AS final_price
 FROM product p
-LEFT JOIN category c ON p.id_category = c.id_category
-LEFT JOIN product_variant v ON p.id_product = v.id_product
-WHERE p.id_product = 1 
-GROUP BY p.id_product, c.category_name;
+JOIN product_variant pv 
+    ON pv.id_product = p.id_product
+WHERE p.id_product = 4
+AND pv.id_variant = 6;
+
 
 SELECT 
-    SUM(
-        (
-            p.base_price + COALESCE(v.additional_price, 0)
-        ) * ci.quantity
-    ) AS sub_total
-FROM cart_item ci
-JOIN cart c ON c.id_cart = ci.id_cart
-JOIN product p ON p.id_product = ci.id_product
-LEFT JOIN product_variant v ON v.id_variant = ci.id_variant
-WHERE c.id_user = 1;
+    product_name,
+    variant_name,
+    quantity,
+    price
+FROM (
+    SELECT 
+        p.product_name,
+        pv.variant_name,
+        10 AS quantity,
+        ((p.base_price + pv.additional_price) * 10) AS price
+    FROM product p
+    JOIN product_variant pv 
+        ON pv.id_product = p.id_product
+    WHERE p.id_product = 4 
+        AND pv.id_variant = 7
+
+    UNION ALL
+
+    SELECT 
+        p.product_name,
+        pv.variant_name,
+        10 AS quantity,
+        ((p.base_price + pv.additional_price) * 10) AS price
+    FROM product p
+    JOIN product_variant pv 
+        ON pv.id_product = p.id_product
+    WHERE p.id_product = 2 
+        AND pv.id_variant = 4
+
+    UNION ALL
+
+    SELECT 
+        p.product_name,
+        pv.variant_name,
+        10 AS quantity,
+        ((p.base_price + pv.additional_price) * 10) AS price
+    FROM product p
+    JOIN product_variant pv 
+        ON pv.id_product = p.id_product
+    WHERE p.id_product = 1 
+        AND pv.id_variant = 2
+
+    UNION ALL
+
+    SELECT 
+        p.product_name,
+        pv.variant_name,
+        10 AS quantity,
+        ((p.base_price + pv.additional_price) * 10) AS price
+    FROM product p
+    JOIN product_variant pv 
+        ON pv.id_product = p.id_product
+    WHERE p.id_product = 5 
+        AND pv.id_variant = 9
+) AS order_items;
 
 
 -- Section Product (Landing Page)
