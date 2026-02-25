52f360b3fbaf958861df709556dd448aa142678b
askadiliya <askadiliyan@gmail.com>
2026-02-25 16:30:31 +0700

    feat: add landing page product and testimonial queries

--- query.sql
+++ query.sql
@@ -21,3 +21,35 @@ JOIN cart c ON c.id_cart = ci.id_cart
 JOIN product p ON p.id_product = ci.id_product
 LEFT JOIN product_variant v ON v.id_variant = ci.id_variant
 WHERE c.id_user = 1;
+
+
+-- Section Product (Landing Page)
+SELECT 
+    p.id_product,
+    p.product_name,
+    p.description,
+    p.base_price,
+    pi.image_path
+FROM product p
+LEFT JOIN product_image pi 
+    ON pi.id_product = p.id_product
+WHERE p.is_active = TRUE
+GROUP BY 
+    p.id_product, 
+    p.product_name, 
+    p.description, 
+    p.base_price,
+    pi.image_path
+LIMIT 4;
+
+-- Section Testimonial
+SELECT 
+    u.name,
+    r.comment,
+    r.rating,
+    r.created_at
+FROM review r
+JOIN users u 
+    ON u.id_user = r.id_user
+ORDER BY r.created_at DESC
+LIMIT 5;
