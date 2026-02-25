b3bc17f2c8071190aa586180dd866bcd62805c4c
askadiliya <askadiliyan@gmail.com>
2026-02-25 09:53:06 +0700

    feat: add table product

--- query.sql
+++ query.sql
@@ -16,3 +16,19 @@ CREATE TABLE category (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+
+CREATE TABLE product (
+    id_product SERIAL PRIMARY KEY,
+    id_category INT NOT NULL,
+    product_name VARCHAR(150) NOT NULL,
+    base_price NUMERIC(12,2) NOT NULL,
+    description TEXT,
+    stock INT DEFAULT 0,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_product_category
+        FOREIGN KEY (id_category)
+        REFERENCES category(id_category)
+        ON DELETE CASCADE
+);
